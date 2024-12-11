ee17ff1017ffdde34875b4df67c525a480ca41e3
Anatoly <contact@anafro.ru>
2024-12-11 14:55:30 +1000

    chore: update navigation routes

--- app/src/main/java/ru/anafro/fitnessapp/navigation/NavigationRoutes.kt
+++ app/src/main/java/ru/anafro/fitnessapp/navigation/NavigationRoutes.kt
@@ -5,4 +5,7 @@ object NavigationRoutes {
     const val REGISTER: String = "register"
     const val LOGIN: String = "login"
     const val ACTIVITY: String = "activity"
+    const val START_ACTIVITY: String = "start-activity"
+    const val CHANGE_PASSWORD: String = "change-password"
+    const val PROFILE: String = "profile"
 }
