b2e92be7219010189babe52d2a0d64b4414bcabb
Anatoly <contact@anafro.ru>
2024-12-16 12:01:09 +1000

    Squashed commit of the following:
    commit cd3150b725d63c20428ba6a2efca5d89d15da15b
    Merge: 88ac268 d4703df
    Author: Anatoly Frolov <44891639+anafro@users.noreply.github.com>
    Date:   Mon Dec 16 04:52:32 2024 +0300
    
        Merge pull request #4 from anafro/revert-3-feature/auth-pages
    
        Revert "Feature: Add activity screen"
    
    commit d4703df323f453af0c88684abdcfcb9a324185f5
    Author: Anatoly Frolov <44891639+anafro@users.noreply.github.com>
    Date:   Mon Dec 16 04:41:53 2024 +0300
    
        Revert "Feature: Add activity screen"
    
    commit 88ac268f699cb450ed79c901fc8928c831223f1f
    Merge: 663b288 6d47242
    Author: Anatoly Frolov <44891639+anafro@users.noreply.github.com>
    Date:   Wed Dec 11 08:01:36 2024 +0300
    
        Merge pull request #3 from anafro/feature/auth-pages
    
        Feature: Add activity screen
    
    commit 6d4724242f2bfb59c556cfc1ebac2160fdd75fd2
    Author: Anatoly <contact@anafro.ru>
    Date:   Wed Dec 11 14:56:43 2024 +1000
    
        feat: add create activity screen
    
    commit fb8c9bec7f1022160cd444fd6afde112ed8cd662
    Author: Anatoly <contact@anafro.ru>
    Date:   Wed Dec 11 14:56:29 2024 +1000
    
        feat: add change password screen
    
    commit ee17ff1017ffdde34875b4df67c525a480ca41e3
    Author: Anatoly <contact@anafro.ru>
    Date:   Wed Dec 11 14:55:30 2024 +1000
    
        chore: update navigation routes
    
    commit 4087ebfb49ebad8a154d2b861a44525922edc5db
    Author: Anatoly <contact@anafro.ru>
    Date:   Wed Dec 11 14:55:16 2024 +1000
    
        feat: add new screens to navigation host
    
    commit 6e9a9b8810f3661d56ba1bef564c8ea217a7642d
    Author: Anatoly <contact@anafro.ru>
    Date:   Wed Dec 11 14:54:55 2024 +1000
    
        refactor: add link component for future refactoring
    
    commit 12aa73c9a401cae7131f3faab6b38721777e071c
    Author: Anatoly <contact@anafro.ru>
    Date:   Wed Dec 11 14:54:34 2024 +1000
    
        feat: add profile screen
    
    commit 663b288d8d1fed3f3b30fbab7406becbbd02e637
    Merge: 44eaf1e f86766f
    Author: Anatoly Frolov <44891639+anafro@users.noreply.github.com>
    Date:   Fri Nov 29 02:30:09 2024 +0300
    
        Merge pull request #2 from anafro/feature/auth-pages
    
        feat: add auth pages

--- app/src/main/java/ru/anafro/fitnessapp/navigation/NavigationRoutes.kt
+++
@@ -1,8 +0,0 @@
-package ru.anafro.fitnessapp.navigation
-
-object NavigationRoutes {
-    const val AUTH: String = "auth"
-    const val REGISTER: String = "register"
-    const val LOGIN: String = "login"
-    const val ACTIVITY: String = "activity"
-}
